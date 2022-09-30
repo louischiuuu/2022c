350d8f436e7b728c4468d7c8c5ecc4de83bc810a
louischiuuu <114201500+louischiuuu@users.noreply.github.com>
2022-09-30 12:07:48 +0800

    week04

---
+++ week04-4 .cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<4; i++ )
+    {
+        printf("印出%d\n",i);
+    }
+    for(int i=1; i<=4; i++ )
+    {
+        printf("人類的版本,印出:%d\n",i);
+    }
+}
+
