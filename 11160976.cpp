7732c57e1b02fc59a2d948a3f35f7a45c556e70b
louischiuuu <114201500+louischiuuu@users.noreply.github.com>
2022-09-30 10:27:44 +0800

    Add files via upload
    week04

---
+++ 11160976.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("請輸入西元年:");
+    int n;
+    scanf("%d",&n);
+    if(n%400==0) printf("閏年");
+    else if( n%100==0) printf("普通年/平年");
+    else if( n%4==0) printf("閏年");
+    else printf("普通年/平年");
+}
