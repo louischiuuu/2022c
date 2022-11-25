024be7d8b2cea6f4c84ed2884c35b7b1d3784ceb
louischiuuu <11160976@me.mcu.edu.tw>
2022-11-25 12:10:47 +0800

    week12

---
+++ week12/week12-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a[2][3]={{10,20,30},{40,50,60}};
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("%\n");
+    }
+}
