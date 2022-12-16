6ed96c17031d51e063d17b3a47dac4ea09411d43
louis930204 <11160976@me.mcu.edu.tw>
2022-12-16 13:14:19 +0800

    week15

---
+++ week15/week15-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    printf ("Hello World\n");
+    char line[]="Hello World";
+
+    printf("整數 %d\n",100);
+    printf("浮點數 %f\n",3.141592653589793238462643383279);
+    printf("%s 字串\n" , line);
+}
+
---
+++ week15/week15-2.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main ()
+{
+    printf("%c : %d\n", 65,65);
+    printf("%c : %d\n", 66,66);
+    printf("%c : %d\n", 67,67);
+    printf("%c : %d\n",'A','A');
+    printf("%c : %d\n",'B','B');
+    printf("%c : %d\n",'C','C');
+    printf("上面用數字 64 及單引號'a'的結果都一樣\n");
+    printf("%c :%d\n",97,97);
+    printf("%c :%d\n",'a','a');
+}
