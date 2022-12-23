fd88a0602631e668f207e0c0c94294eb133b983b
louischiuuu <114201500+louischiuuu@users.noreply.github.com>
2022-12-23 12:00:32 +0800

    week16

---
+++ week16-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char line[300];
+    char*p=line;
+    int n=10;
+    int *p2=&n;
+    float f=3.1415926;
+    float *p3=&f;
+    char c='A';
+    char *p4=&c;
+}
